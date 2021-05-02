b0fc344826175e9e04e687f42cf094c9a8e69368
kylepmccarthy <kylemcca@upenn.edu>
2021-05-02 14:38:05 -0400

    Update main.js

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -9,8 +9,8 @@ var map = L.map('map', {
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
+  minZoom: 10,
+  maxZoom: 18,
   ext: 'png'
 }).addTo(map);
 
