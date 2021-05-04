572f60f16eed9012cf8e90e3b2d0584bdd57addd
kylepmccarthy <kylemcca@upenn.edu>
2021-05-04 11:10:14 -0400

    6

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -31,7 +31,7 @@ var alteration1 = "https://raw.githubusercontent.com/kylepmccarthy/675Final/main
 var plum2 = "https://raw.githubusercontent.com/kylepmccarthy/675Final/main/Data/PlumPost2.geojson "
 var Mechanical = "https://raw.githubusercontent.com/kylepmccarthy/675Final/main/Data/Mechanical.geojson"
 
-var results = "https://raw.githubusercontent.com/kylepmccarthy/675Final/main/Data/Scen4.geojson"
+var results = "https://raw.githubusercontent.com/kylepmccarthy/675Final/main/Data/Scen5.geojson"
 
 var featureGroups; 
 var featureEL;
